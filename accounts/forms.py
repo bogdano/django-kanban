389906194cef7a20e1ba691d29f2e9ac44a5a796
terrin-martin <terrinmartin@icloud.com>
2023-11-16 11:39:46 -0700

    Added the Role field

--- accounts/forms.py
+++ accounts/forms.py
@@ -6,7 +6,7 @@ from .models import CustomUser
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = CustomUser
-        fields = UserCreationForm.Meta.fields + ("age",)
+        fields = UserCreationForm.Meta.fields + ("role",)
 
 
 class CustomUserChangeForm(UserChangeForm):
