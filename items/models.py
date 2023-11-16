7e8c86a3e4654031773fbdae83eb0b26e30ec666
bogdan <bogdan.bokovic@umontana.edu>
2023-11-16 11:57:19 -0700

    edited items models, and commented code

--- items/models.py
+++ items/models.py
@@ -1,11 +1,20 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+# the base Item model, for all board items
 class Item(models.Model):
+    # text content
     content = models.CharField(max_length=200)
+    # boolean to indicate if item is checked off (we can use this with HTML checkboxes)
     checked = models.BooleanField(default=False)
+    # automatic timestamp for when item was created
     date_added = models.DateTimeField(auto_now_add=True)  
+    # an author field which is a foreign key to the User model which Terrin created
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # an automatic timestamp for when item was last updated by a user (will take care of this in views.py)
+    last_updated = models.DateTimeField(auto_now=True)
+    # a foreign key to the User model which Terrin created, updated each time an item is moved to a new board
+    updated_by = models.ForeignKey('auth.User', related_name='updated_by', null=True)
 
     def __str__(self):
         return self.content
@@ -13,42 +22,34 @@ class Item(models.Model):
     def get_absolute_url(self):
         return reverse("item_detail", kwargs={"pk": self.pk})
     
-class IdeasList(models.Model):
+# this is a class for all the boards
+class BoardList(models.Model):
+    # these are the types of boards we'll have
+    LIST_TYPES = [
+        ('IDEAS', 'Ideas'),
+        ('TODO', 'Todo'),
+        ('DOING', 'Doing'),
+        ('DONE', 'Done'),
+    ]
+    # the name of the board, a string
     name = models.CharField(max_length=200)
-    items = models.ManyToManyField(Item, related_name="ideaslist")
+    # the type of board, from the array above
+    list_type = models.CharField(max_length=5, choices=LIST_TYPES)
+    # defining a many-to-many relationship between the Item model and the BoardList model
+    items = models.ManyToManyField(Item, related_name="boardlist")
 
     def __str__(self):
-        return self.name
+        return f"{self.get_list_type_display()} - {self.name}"
 
-    def get_absolute_url(self):
-        return reverse("ideas_detail", kwargs={"pk": self.pk})
-
-class TodoList(models.Model):
-    name = models.CharField(max_length=200)
-    items = models.ManyToManyField(Item, related_name="todolist")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("todo_detail", kwargs={"pk": self.pk})
-    
-class DoingList(models.Model):
-    name = models.CharField(max_length=200)
-    items = models.ManyToManyField(Item, related_name="doinglist")
+class Activity(models.Model):
+    # activity records which user moved, updated, or deleted an item
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    # activity records which item was acted upon
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    # empty string which will store the name of the board to which an item was moved
+    destination_board = models.CharField(max_length=200)
+    # automatic timestamp for when activity was created
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("doing_detail", kwargs={"pk": self.pk})
-
-class DoneList(models.Model):
-    name = models.CharField(max_length=200)
-    items = models.ManyToManyField(Item, related_name="donelist")
-
-    def __str__(self):
-        return self.name
-
-    def get_absolute_url(self):
-        return reverse("done_detail", kwargs={"pk": self.pk})
+        return f"{self.user} moved {self.item} to {self.destination_board}"
